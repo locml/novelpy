6437d43edca2eff10af3d578b4eef49e7f943841
Lộc <108453991+locml@users.noreply.github.com>
2022-07-11 13:24:18 +0700

    Delete data directory

--- data/about.py
+++
@@ -1,8 +0,0 @@
-# A unused script.
-def about():
-    return print(
-        "Thanks you for playing this.",
-        "Made with Pygame SDL2.",
-        "Played, Tested, Development with Windows and Python 3."
-        )
-        
